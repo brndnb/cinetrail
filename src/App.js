ff156319caad4a0bb5eac773bdeff143cd2c93b2
Daniel Belleza <bellezabrndn@gmail.com>
2023-02-17 11:52:51 -0500

    cinetrail week1 done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Homepage from './pages/Homepage/Homepage';
+import ThemeContextProvider from './contexts/ThemeContext';
+
 
 function App() {
   return (
     <div>
-     
+      <ThemeContextProvider>
+       <Header />
+       <Homepage />
+       <Footer />
+      </ThemeContextProvider>
     </div>
   );
 }
